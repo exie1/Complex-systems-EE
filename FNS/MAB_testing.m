146b0c19b16a3aaa4ee47c8bdc4a20a8d1903951
exie1 <exie5374@uni.sydney.edu.au>
2022-03-27 17:11:37 +1100

    Fixed MAB simulation
    Properly functioning now. Performance ranges between pure exploration and pure exploitation (still lots of exploration occuring)

--- FNS/MAB_testing.m
+++ FNS/MAB_testing.m
@@ -16,30 +16,65 @@ p.location = [R*cos(theta),R*sin(theta);
     R*cos(theta+4*pi/3),R*sin(theta+4*pi/3)];
 p.depth = [1,1,1];
 
-p.radius2 = [1,1,1].^2;
-p.rewardMu = [3,4,5];       % should initialise this as sampled values
-p.rewardSig = [5,4,3]/2;
+p.radius2 = [1,1,1].^2;     % automatically rescaled later
+p.rewardMu = [3,4,5];       
+p.rewardSig = [3,4,5]/3;
+
 
 tic
-[X,t,history] = fHMC_MAB(T,a,p,window,num_parallel);
+[X,t,history,history_rad] = fHMC_MAB(T,a,p,window,num_parallel);
 toc
 
-regret = 1 - (sum(history(2,:))/max(p.rewardMu)*MAB_steps);
+optimal = max(p.rewardMu)*MAB_steps;
+regret = 1 - (sum(history(2,:))/optimal);
+[cnt_unique, uniq] = hist(history(1,:),unique(history(1,:)));
+disp('Number of times each option is sampled + overall regret')
+disp([cnt_unique,regret])
 %%
 figure
-subplot(1,2,1)
+subplot(2,3,1)
 plot(history(1,:))
 xlabel('Step')
 ylabel('Chosen option')
 
-subplot(1,2,2)
+subplot(2,3,2)
 histogram2(X(1,:,1),X(2,:,1),'Normalization','probability','FaceColor','flat')
 xlabel('x')
 ylabel('y')
 
-%%
+subplot(2,3,3)
+for plt = 1:3
+    hold on
+    plot(history_rad(plt,:))
+end
+xlabel('MAB step')
+ylabel('Radius')
 
-figure
-oneOpt = history(2,:) .* (history(1,:) == 3);
-histogram(oneOpt)
-(5*500 - sum(history(2,:)))/(5*500)
+pdf_range = linspace(min(history(2,:)),max(history(2,:)),200);
+disp('Mean and std of simulated payoff distribution')
+for plt = 1:3
+    subplot(2,3,3+plt)
+    oneOpt = history(2,:).*(history(1,:) == plt);  % Payoffs from option __ 
+    hold on
+    histogram(oneOpt(oneOpt~=0),'Normalization','probability')
+    plot(pdf_range,normpdf(pdf_range,p.rewardMu(plt),p.rewardSig(plt)),'lineWidth',2)
+    xlabel('Payoff')
+    ylabel('Probability')
+    disp([mean(oneOpt(oneOpt~=0)),std(oneOpt(oneOpt~=0))])
+end
+
+
+%% Find total duration?
+
+numTries = 1e5;
+pureEE_lst = zeros(2,numTries);
+for testing = 1:numTries
+    reward_i = p.rewardSig.*randn(1,3)+p.rewardMu;
+    [~,chosen1] = max(reward_i);
+    [~,chosen2] = max(rand(1,3));
+    pureEE_lst(1,testing) = p.rewardMu(chosen1)*MAB_steps;
+    pureEE_lst(2,testing) = p.rewardMu(chosen2)*MAB_steps;
+end
+disp("Pure exploitation and pure exploration mean regret:")
+disp([1 - (mean(pureEE_lst(1,:)))/optimal, ...
+    1 - (mean(pureEE_lst(2,:)))/optimal])
