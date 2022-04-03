f52db1308101ea15c80b77fd19d9ff14745390f3
exie1 <exie5374@uni.sydney.edu.au>
2022-04-03 13:11:34 +1000

    Added averaging + classes
    Averaged code is bugged, reward setup changed

--- FNS/MAB_testing.m
+++ FNS/MAB_testing.m
@@ -17,8 +17,10 @@ p.location = [R*cos(theta),R*sin(theta);
 p.depth = [1,1,1];
 
 p.radius2 = [1,1,1].^2;
-p.rewardMu = [3,4,5];       % should initialise this as sampled values
-p.rewardSig = [5,4,3]/2;
+% p.rewardMu = [3,4,5];       % should initialise this as sampled values
+% p.rewardSig = [5,4,3]/2;
+p.rewardMu = 2 + 3*(1:3);
+p.rewardSig = 0.5*p.rewardMu;
 
 tic
 [X,t,history,history_rad] = fHMC_MAB(T,a,p,window,num_parallel);
@@ -35,11 +37,14 @@ subplot(2,3,1)
 plot(history(1,:))
 xlabel('Step')
 ylabel('Chosen option')
+title('Choice history')
+xlim([0,MAB_steps])
 
 subplot(2,3,2)
 histogram2(X(1,:,1),X(2,:,1),'Normalization','probability','FaceColor','flat')
 xlabel('x')
 ylabel('y')
+title('Sampled distribution')
 
 subplot(2,3,3)
 for plt = 1:3
@@ -48,6 +53,8 @@ for plt = 1:3
 end
 xlabel('MAB step')
 ylabel('Radius')
+legend('Well 1', 'Well 2', 'Well 3')
+title('Radius history')
 
 pdf_range = linspace(min(history(2,:)),max(history(2,:)),200);
 disp('Mean and std of simulated payoff distribution')
@@ -59,6 +66,7 @@ for plt = 1:3
     plot(pdf_range,normpdf(pdf_range,p.rewardMu(plt),p.rewardSig(plt)),'lineWidth',2)
     xlabel('Payoff')
     ylabel('Probability')
+    title(['Well ',num2str(plt),' payoff, sampled ' ,num2str(cnt_unique(plt)),' times'])
     disp([mean(oneOpt(oneOpt~=0)),std(oneOpt(oneOpt~=0))])
 end
 
@@ -77,3 +85,57 @@ end
 disp("Pure exploitation and pure exploration mean regret:")
 disp([1 - (mean(pureEE_lst(1,:)))/optimal, ...
     1 - (mean(pureEE_lst(2,:)))/optimal])
+
+
+%% Looping stuff
+
+reco = [];
+for es = 1:20
+    p.rewardMu = 2 + 3*(1:3);
+    p.rewardSig = 0.5*p.rewardMu;
+
+    [X,t,history,history_rad] = fHMC_MAB(T,a,p,window,num_parallel);
+
+    optimal = max(p.rewardMu)*MAB_steps;
+    regret = 1 - (sum(history(2,:))/optimal);
+    [cnt_unique, uniq] = hist(history(1,:),unique(history(1,:)));
+    disp('Number of times each option is sampled + overall regret')
+    disp([cnt_unique,regret])
+    reco = [reco, regret];
+end
+  
+
+%%
+a = 1.3; % Levy tail exponent
+p.beta = 1; % beta coefficient
+p.gam = 1; % strength of the Levy noise
+p.dt = 1e-3; % integration time step
+T = 1e3; % simulation time
+MAB_steps = 500;
+window = T/p.dt/MAB_steps;
+num_parallel = 30;
+
+%--------Defining stimuli and running simulation--------------------
+
+R = pi/2;
+theta = pi/2;   % for triangle stim
+p.location = [R*cos(theta),R*sin(theta); 
+    R*cos(theta+2*pi/3),R*sin(theta+2*pi/3);
+    R*cos(theta+4*pi/3),R*sin(theta+4*pi/3)];
+p.depth = [1,1,1];
+
+p.radius2 = [1,1,1].^2;
+% p.rewardMu = [3,4,5];       % should initialise this as sampled values
+% p.rewardSig = [5,4,3]/2;
+p.rewardMu = 2 + 3*(1:3);
+p.rewardSig = 0.5*p.rewardMu;
+
+tic
+[X,t,history_rewards,history_choices] = fHMC_MAB_avgd(T,a,p,window,num_parallel);
+toc
+
+optimal = max(p.rewardMu)*MAB_steps;
+regret = 1 - (sum(history_rewards,2)/optimal)
+% [cnt_unique, uniq] = hist(history(avg,:),unique(history(1,:)));
+% disp('Number of times each option is sampled + overall regret')
+% disp([cnt_unique,regret])
--- FNS/fHMC_MAB_avgd.m
+++ FNS/fHMC_MAB_avgd.m
@@ -1,4 +1,4 @@
-function [X,t,history,history_rad] = fHMC_MAB(T,a,p,window,avg)
+function [X,t,history_rewards,history_choices] = fHMC_MAB_avgd(T,a,p,window,avg)
     % Same fHMC algorithm, but optimised for multi-armed bandit
     % simulations. Change width for bottom-up attention, change depth for
     % top-down attention, change Levy noise for random exploration.
@@ -13,32 +13,36 @@ function [X,t,history,history_rad] = fHMC_MAB(T,a,p,window,avg)
     numWells = length(p.rewardMu);
     
     
-    x = zeros(2,avg)+[1e-10;0]; % initial condition for each parallel sim
-    v = zeros(2,avg)+[1;0];     % ^ but velocity
+    x = zeros(avg,2)+[1e-10,0]; % initial condition for each parallel sim
+    v = zeros(avg,2)+[0,1];     % ^ but velocity
 
     ca = gamma(a-1)/(gamma(a/2).^2);    % fractional derivative approx
 
     % Initialising recording arrays
-    expectation = zeros(1,numWells);
-    history = zeros(2,round(n/window)+numWells); 
-    history_rad = zeros(numWells,round(n/window));
+    expectation = zeros(avg,numWells);
+    history_rewards = zeros(avg,round(n/window)+numWells);
+    history_choices = zeros(avg,round(n/window)+numWells);
     X = zeros(m,n,avg);
     
     % Sampling each well and adjusting radius
-    history(:,1:numWells) = [1:numWells; p.rewardSig.*randn(1,3)+p.rewardMu];
-    weights = softmax1(history(2,1:numWells));
+    for trial = 1:avg
+        history_choices(trial,1:numWells) = 1:numWells;
+        history_rewards(trial,1:numWells) = p.rewardSig.*randn(1,numWells)+p.rewardMu;
+    end
+    weights = softmax1(history_rewards(:,1:numWells));
     p.radius2 = (max_rad*weights).^2;
-    disp(sqrt(p.radius2))
+%     p.depth = sqrt(p.radius2); % sqrt? + don't initialise?
+    disp(weights)
     
     counter = 1+numWells;
     for i = 1:window:n      % num steps separated into time windows   
         for w = i:i+window-1
             f = getPotential(x,p);
 
-            dL = stblrnd(a,0,p.gam,0,[2,avg]); 
-            r = sqrt(sum(dL.*dL,1)); %step length
-            th = rand(1,avg)*2*pi;
-            g = r.*[cos(th);sin(th)];
+            dL = stblrnd(a,0,p.gam,0,[avg,2]); 
+            r = sqrt(sum(dL.*dL,2)); %step length
+            th = rand(avg,1)*2*pi;
+            g = r.*[cos(th),sin(th)];
 
             % Stochastic fractional Hamiltonian Monte Carlo
             vnew = v + p.beta*ca*f*dt;
@@ -47,28 +51,27 @@ function [X,t,history,history_rad] = fHMC_MAB(T,a,p,window,avg)
             v = vnew;
 
             x = wrapToPi(x); % apply periodic boundary to avoid run-away
-            X(:,w,:) = x;    % record position
+            X(:,w,:) = x';    % record position
         end
-        chosen = proximityCheck(X(:,i:w,:),p.location);
-        history(1,counter) = chosen;
-        history(2,counter) = p.rewardSig(chosen)*randn()+p.rewardMu(chosen);
-        history_rad(:,counter) = p.radius2';
-        
+        chosen = proximityCheck(X(:,i:w,:),p.location)';
+        history_choices(:,counter) = chosen;
+        history_rewards(:,counter) = p.rewardSig(chosen)'.*randn(avg,1) + p.rewardMu(chosen)';
+                
         % Updating well parameters according to sampled history
         for opt = 1:length(p.rewardMu)
-            rewards = history(2,1:counter) .* (history(1,1:counter) == opt);
-            expectation(opt) = mean(rewards(rewards~=0));
+            rewards = history_rewards(:,1:counter).*(history_choices(:,1:counter)==opt);
+            expectation(:,opt) = mean(rewards(rewards~=0),1);
         end
         weights = softmax1(expectation);
         p.radius2 = (max_rad*weights).^2;
-        p.depth = p.radius2; %sqrt or no?
+        p.depth = p.radius2;
         counter = counter + 1;
     end
 end
 
 function weights = softmax1(vec)
-    % Softmax function
-    weights = exp(vec)/sum(exp(vec));
+    % Rowwise softmax function
+    weights = exp(vec)./sum(exp(vec),2);
 end
 
 function f = getPotential(x,p)
@@ -76,12 +79,12 @@ function f = getPotential(x,p)
     fx = 0;
     fy = 0;     
     for j = 1:size(p.location,1)
-        pot = p.depth(j)*(((x(1,:) - p.location(j,1)).^2 + ...
-            (x(2,:) - p.location(j,2)).^2)/p.radius2(j) - 1);
-        gradx = (x(1,:)-p.location(j,1))*2*p.depth(j)/p.radius2(j);
-        grady = (x(2,:)-p.location(j,2))*2*p.depth(j)/p.radius2(j);
+        pot = p.depth(:,j).*(((x(:,1) - p.location(j,1)).^2 + ...
+            (x(:,1) - p.location(j,2)).^2)./p.radius2(:,j) - 1);
+        gradx = (x(:,1)-p.location(j,1))*2.*p.depth(:,j)./p.radius2(:,j);
+        grady = (x(:,2)-p.location(j,2))*2.*p.depth(:,j)./p.radius2(:,j);
         fx = fx + gradx.*(pot<=0);
         fy = fy + grady.*(pot<=0);
     end
-    f = -[fx;fy];
+    f = -[fx,fy];
 end
